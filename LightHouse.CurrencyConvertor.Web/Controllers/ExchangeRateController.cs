97c30b2c6c131c6d50008840d6c6b662757f68ab
IanFelton <ian@ianfelton.com>
2016-01-30 13:51:43 -0600

    Create webapi controller and add ninject DI

---
+++ LightHouse.CurrencyConvertor.Web/Controllers/ExchangeRateController.cs
@@ -0,0 +1,35 @@
+﻿using Currency.Services;
+using Lighthouse.CurrencyConvertor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LightHouseCurrencyConvertor.Controllers
+{
+    [Authorize]
+    public class ExchangeRateController : ApiController
+    {
+
+        private IExchangeRateService exchangeRateService;
+
+        public ExchangeRateController(IExchangeRateService exchangeRateService)
+        {
+            this.exchangeRateService = exchangeRateService;
+        }
+
+        // GET api/values/5
+        public IEnumerable<ExchangeRate> Get()
+        {
+            return exchangeRateService.GetRates();
+        }
+
+        // POST api/values
+        public void Post([FromBody]IEnumerable<ExchangeRate> value)
+        {
+            exchangeRateService.UpdateRates(value);
+        }
+    }
+}
