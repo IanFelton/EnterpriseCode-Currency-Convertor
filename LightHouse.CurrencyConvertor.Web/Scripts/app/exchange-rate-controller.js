7ba864dfb99fe93377e495900a98bb069d5314dc
IanFelton <ian@ianfelton.com>
2016-01-30 23:49:44 -0600

    Move web api to service. Add controls to complete a conversion

--- LightHouse.CurrencyConvertor.Web/Scripts/app/exchange-rate-controller.js
+++ LightHouse.CurrencyConvertor.Web/Scripts/app/exchange-rate-controller.js
@@ -1,16 +1,45 @@
-﻿angular.module('ExchangeRateApp', [])
-    .controller('ExchangeRateCtrl', function ($scope, $http) {
+﻿angular.module('ExchangeRateApp', ['exchange-rate-service'])
+    .controller('ExchangeRateCtrl', ['exchangeRates', '$scope', function (exchangeRates, $scope) {
+
+        $scope.success = false;
+        $scope.error = false;
+
         $scope.to = '';
         $scope.from = '';
         $scope.amount = 0.0;
-        $scope.response;
-        $scope.result = function () {
-            return 0;
-        };
+        $scope.result;
 
-        $http.get("/api/exchangerate").success(function (data, status, headers, config) {
-            $scope.response = data.results;
-        }).error(function (data, status, headers, config) {
-            $scope.message = "Oops... something went wrong";
-        });
-    });
+        var getData = exchangeRates.getRateData();
+
+        getData.then(
+            function (results) {
+                $scope.response = results.data.results;
+                $scope.success = true;
+            },
+            function (error) {
+                $scope.message = error;
+                $scope.error = true;
+
+            });
+
+        $scope.countries = function () {
+            $.each($scope.response, function (i, v) {
+                if (v.base == $scope.from) {
+                    $scope.toCountries = v.rates;
+                }
+            });
+        }
+
+        $scope.setRate = function () {
+            $.each($scope.toCountries, function (i, v) {
+                if (i == $scope.to) {
+                    $scope.rate = v;
+                }
+            });
+        }
+
+        $scope.calculate = function () {
+            $scope.result = $scope.amount * $scope.rate;
+        }
+
+    }]);
---
+++ LightHouse.CurrencyConvertor.Web/Scripts/app/exchange-rate-service.js
@@ -0,0 +1,22 @@
+﻿angular.module('exchange-rate-service', [])
+    .service('exchangeRates', ['$http', '$q', function ($http, $q) {
+        var deferObject,
+        getRateData = {
+            
+            getRateData: function () {
+                var promise = $http.get("/api/exchangerate"),
+                    deferObject = deferObject || $q.defer();
+
+                promise.then(
+                    function (result) {
+                        deferObject.resolve(result);
+                    },
+                    function (error) {
+                        deferObject.reject(error);
+                    });
+                return deferObject.promise;
+            }
+        };
+
+        return  getRateData;
+    }]);
